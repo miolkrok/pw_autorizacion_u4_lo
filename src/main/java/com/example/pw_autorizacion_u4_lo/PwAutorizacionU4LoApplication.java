b4522e77766ca1387b4c7f72dcbe9b514a269e25
luis <luis@gmail.com>
2023-08-09 19:47:25 -0500

    first commit

---
+++ src/main/java/com/example/pw_autorizacion_u4_lo/PwAutorizacionU4LoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pw_autorizacion_u4_lo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4LoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4LoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pw_autorizacion_u4_lo/PwAutorizacionU4LoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pw_autorizacion_u4_lo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4LoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
